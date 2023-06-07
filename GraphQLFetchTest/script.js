0b329ebf7914c04258dc5b2f02e9486385c498aa
suyash.vyas <suyashm23@gmail.com>
2023-06-08 01:14:29 +0530

    Restructed file system, added fetching graphQL query for Sprinklr Atlassian

---
+++ GraphQLFetchTest/script.js
@@ -0,0 +1,39 @@
+const urlGitLab = 'https://prod-gitlab.sprinklr.com/api/graphql';
+const urlAtlassian = 'https://sprinklr.atlassian.net/gateway/api/graphql';
+
+const inputGitLab = document.getElementById('inputGitLab');
+const buttonGitLab = document.getElementById('buttonGitLab');
+const textareaGitLab = document.getElementById('textareaGitLab');
+
+const inputAtlassian = document.getElementById('inputAtlassian');
+const buttonAtlassian = document.getElementById('buttonAtlassian')
+const textareaAtlassian = document.getElementById('textareaAtlassian');
+
+function runQuery(input, textarea, url) {
+
+    const token = input.value;
+    const query = textarea.value || textarea.placeholder;
+
+    fetch(url, {
+    method: 'POST',
+    headers: {
+        //'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+        query: query
+      }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Process the response data
+        console.log(data.data);
+    })
+    .catch(error => {
+        // Handle any errors
+        console.error(error);
+    });
+}
+
+buttonGitLab.addEventListener('click', () => runQuery(inputGitLab, textareaGitLab, urlGitLab));
+buttonAtlassian.addEventListener('click', () => runQuery(inputAtlassian, textareaAtlassian, urlAtlassian));
