770576cae52d80958202197e0dd4664ffe61afd0
Suyash Vyas <suyash.vyas@sprinklr.com>
2024-02-20 11:03:49 +0530

    Removed: Previous static page

--- site/1/resource/amd/lwr-error-shim.js/v/0_11_13/lwr-error-shim.js
+++
@@ -1,8 +0,0 @@
-/**
-* Copyright (c) 2021, salesforce.com, inc.
-* All rights reserved.
-* SPDX-License-Identifier: MIT
-* For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
-*/
-/* LWR Error Shim v0.11.13 */
-!function(){"use strict";if(!(globalThis.LWR&&globalThis.LWR.define)){const o=new Error("The LWR application failed to bootstrap");if(!globalThis.LWR||!globalThis.LWR.onError)throw o;globalThis.LWR.onError(o)}}();
